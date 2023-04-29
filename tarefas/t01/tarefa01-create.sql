bcc19d26c09416ed96b647d84bcf5e44eb80c3e8
marlemM <marlem.magno@gmail.com>
2023-04-29 02:39:11 -0300

    tarefa01-create #1

--- tarefas/t01/tarefa01-create.sql
+++ tarefas/t01/tarefa01-create.sql
@@ -1,41 +1,51 @@
 CREATE TABLE funcionario (
-    codigo INTEGER PRIMARY KEY,
-    nome TEXT,
-    sexo CHAR(1),
-    dt_nasc DATE,
-    salario DECIMAL(10, 2),
-    cod_depto INTEGER
+	codigo serial,
+	nome varchar(50),
+	sexo char(1),
+	dt_nasc date,
+	salario decimal(10,2),
+	cod_depto int,
+	PRIMARY KEY (codigo)
 );
 
 CREATE TABLE departamento (
-    codigo INTEGER PRIMARY KEY,
-    descricao TEXT,
-    cod_gerente INTEGER
+	codigo serial,
+	descricao varchar(50),
+	cod_gerente int,
+	PRIMARY KEY (codigo),
+	FOREIGN KEY (cod_gerente) REFERENCES funcionario(codigo) on delete set null on update cascade
 );
 
+ALTER TABLE funcionario ADD FOREIGN KEY(cod_depto) REFERENCES departamento(codigo);
+
 CREATE TABLE projeto (
-    codigo INTEGER PRIMARY KEY,
-    nome TEXT,
-    descricao TEXT,
-    cod_depto INTEGER,
-    cod_responsavel INTEGER,
-    data_inicio DATE,
-    data_fim DATE
+	codigo serial,
+	nome varchar(50),
+	descricao varchar(250),
+	cod_responsavel int,
+	cod_depto int,
+	data_inicio date, 
+	data_fim date,
+	PRIMARY KEY (codigo),
+	FOREIGN KEY (cod_responsavel) REFERENCES funcionario(codigo) on delete set null on update cascade,
+	FOREIGN KEY (cod_depto) REFERENCES departamento(codigo) on delete set null on update cascade
 );
 
 CREATE TABLE atividade (
-    codigo INTEGER PRIMARY KEY,
-    nome TEXT,
-    descricao TEXT,
-    cod_responsavel INTEGER,
-    data_inicio DATE,
-    data_fim DATE
+	codigo serial,
+    nome varchar(50),
+	descricao varchar(250),
+	cod_responsavel int,
+	data_inicio date, 
+	data_fim date,
+	PRIMARY KEY (codigo),
+	FOREIGN KEY (cod_responsavel) REFERENCES funcionario(codigo) on delete set null on update cascade
 );
 
 CREATE TABLE atividade_projeto (
-    cod_projeto INTEGER,
-    cod_atividade INTEGER,
-    PRIMARY KEY (cod_projeto, cod_atividade),
-    FOREIGN KEY (cod_projeto) REFERENCES projeto(codigo),
-    FOREIGN KEY (cod_atividade) REFERENCES atividade(codigo)
+	cod_projeto int,
+	cod_atividade int,
+	PRIMARY KEY (cod_projeto, cod_atividade),
+	FOREIGN KEY (cod_projeto) REFERENCES projeto(codigo) on delete set null on update cascade,
+	FOREIGN KEY (cod_atividade) REFERENCES atividade(codigo) on delete set null on update cascade
 );
