c9ea629d8abbdb6e1bbb74b642928486b1eeac6b
marlemM <marlem.magno@gmail.com>
2023-04-29 02:45:45 -0300

    tarefa01-insert #2

--- tarefas/t01/tarefa01-inserts.sql
+++ tarefas/t01/tarefa01-inserts.sql
@@ -1,1 +1,42 @@
-commit
+-- Inserscao de dados na tabela "departamento"
+INSERT INTO departamento (descricao, cod_gerente)
+VALUES ('Vendas', NULL),
+       ('Marketing', NULL),
+       ('TI', NULL),
+       ('Recursos Humanos', NULL),
+       ('Financeiro', NULL);
+      
+-- Inserscao de dados na tabela "funcionario"
+INSERT INTO funcionario (nome, sexo, dt_nasc, salario, cod_depto)
+VALUES ('Taciano', 'M', '1980-07-13', 3000.00, 1),
+       ('Marcia', 'F', '2003-03-13', 2500.00, 1),
+       ('Marlem', 'M', '1999-10-31', 4000.00, 2),
+       ('Daniel', 'M', '2000-06-16', 3500.00, 3),
+       ('Marlon', 'M', '2004-01-20', 2800.00, 2);
+
+update departamento set cod_gerente = 1 where codigo = 1;
+update departamento set cod_gerente = 2 where codigo = 2;
+update departamento set cod_gerente = 1 where codigo = 3;
+update departamento set cod_gerente = 2 where codigo = 4;
+update departamento set cod_gerente = 2 where codigo = 5;
+
+-- Inserscao de dados na tabela "projeto"
+INSERT INTO projeto (nome, descricao, data_inicio, data_fim, cod_depto, cod_responsavel)
+VALUES ('Projeto A', 'Descricao do Projeto A', '2023-01-01', '2023-07-01', 1, 1),
+       ('Projeto B', 'Descricao do Projeto B', '2023-02-01', '2023-08-01', 2, 3),
+       ('Projeto C', 'Descricao do Projeto C', '2023-03-01', '2023-09-01', 3, 5),
+       ('Projeto D', 'Descricao do Projeto D', '2023-04-01', '2023-10-01', 4, 2),
+       ('Projeto E', 'Descricao do Projeto E', '2023-05-01', '2023-11-01', 5, 4);
+
+
+-- Insercao de dados na tabela "atividade"
+INSERT INTO atividade (nome, descricao, data_inicio, data_fim, cod_responsavel)
+VALUES ('Atividade 1', 'Descricao da Atividade 1', '2023-01-01', '2023-02-01', 1),
+       ('Atividade 2', 'Descricao da Atividade 2', '2023-02-01', '2023-03-01', 3),
+       ('Atividade 3', 'Descricao da Atividade 3', '2023-03-01', '2023-04-01', 5),
+       ('Atividade 4', 'Descricao da Atividade 4', '2023-04-01', '2023-05-01', 2),
+       ('Atividade 5', 'Descricao da Atividade 5', '2023-05-01', '2023-06-01', 4);
+
+-- Inserscao de dados na tabela "atividade_projeto"
+INSERT INTO atividade_projeto (cod_projeto, cod_atividade)
+VALUES (1, 1), (2, 2), (3, 3), (4, 4), (5, 5);
